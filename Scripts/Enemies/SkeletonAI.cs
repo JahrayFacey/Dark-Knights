0a7c63687f236912322f93e6a9cba66264482e38
JahrayFacey <99982162+JahrayFacey@users.noreply.github.com>
2025-03-10 14:21:26 +0000

    Add files via upload

--- Scripts/Enemies/SkeletonAI.cs
+++ Scripts/Enemies/SkeletonAI.cs
@@ -7,22 +7,55 @@ public class SkeletonAI : MonoBehaviour
 {
     private NavMeshAgent nav;
     public GameObject player;
-
+    public Animator anim;
     private float updateTime = 0;
+
+    private bool IsAttacking;
+    private PlayerData data;
+    public float minDamage;
+    public float maxDamage;
     // Start is called before the first frame update
     public void Start()
     {
         nav = GetComponent<NavMeshAgent>();
+        data = FindObjectOfType<PlayerData>();
         //Invoke("FindTarget", 2);
     }      
+
+    
 
     public void Update()
     {
         updateTime += Time.deltaTime;
+
+        float dist = Vector3.Distance(this.transform.position, player.transform.position);
+        if(dist <= 3)
+        {
+            if(!IsAttacking){
+
+            }
+            StartCoroutine(Attack());
+        }
+        else
+        {
+            anim.SetBool("Attack", false);
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        if(!IsAttacking){
+            IsAttacking = true;
+            anim.SetBool("Attack", false);
+            yield return new WaitForSeconds(1.2f);
+            data.TakeDamage(Random.Range(minDamage, maxDamage));
+            IsAttacking = false;
+        }
     }
 
     private void LateUpdate()
     {
+        transform.LookAt(player.transform);
         if(updateTime > 2)
         {
             nav.destination = player.transform.position;
